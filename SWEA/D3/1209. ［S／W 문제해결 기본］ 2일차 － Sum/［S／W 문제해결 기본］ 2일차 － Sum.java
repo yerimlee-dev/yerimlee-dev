20d3256e3a7a2645a1edc47cb7e8fed7e7d61494
Yerim Lee <274205512+yerimlee-dev@users.noreply.github.com>
2026-04-15 01:32:50 +0900

    [D3] Title: [S/W 문제해결 기본] 2일차 - Sum, Time: 129 ms, Memory: 37,112 KB -BaekjoonHub

--- SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
+++ SWEA/D3/1209. ［S／W 문제해결 기본］ 2일차 － Sum/［S／W 문제해결 기본］ 2일차 － Sum.java
@@ -6,51 +6,40 @@ public class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
  
-        int[][] arr = new int[100][100]; // 2차원 배열 선언
- 
         for (int tc = 1; tc <= 10; tc++) {
-            int tcNum = Integer.parseInt(br.readLine()); // 테스트케이스 번호 입력
+            int n = Integer.parseInt(br.readLine());
+            int[][] map = new int[100][100];
+            int rowSum = 0, rowMax = 0;
+            int colSum = 0, colMax = 0;
+            int croSum1 = 0, croSum2 = 0, croMax = 0;
  
-            int rowSum = 0, rowMax = 0; // 행의 합, 행의 합 최댓값
-            int colSum = 0, colMax = 0; // 열의 합, 열의 합 최댓값
-            int croSum1 = 0, croSum2 = 0, croMax = 0; // 대각선의 합, 대각선의 합 최댓값
-            int max = 0; // rowMax, colMax, croMax 중 최댓값
- 
-            // arr 2차원 배열의 각 행값 입력, rowSum 및 rowMax 구하기
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < map.length; i++) {
                 rowSum = 0;
- 
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int j = 0; j < 100; j++) {
-                    arr[i][j] = Integer.parseInt(st.nextToken());
-                    rowSum += arr[i][j]; // 각 행의 값 합산
+                for (int j = 0; j < map.length; j++) {
+                    map[i][j] = Integer.parseInt(st.nextToken());
+                    rowSum += map[i][j];
                 }
- 
                 rowMax = Math.max(rowSum, rowMax);
             }
  
-            // colSum 및 colMax 구하기
-            for (int i = 0; i < 100; i++) {
+            for (int i = 0; i < map.length; i++) {
                 colSum = 0;
- 
-                for (int j = 0; j < 100; j++) {
-                    colSum += arr[j][i];
+                for (int j = 0; j < map.length; j++) {
+                    colSum += map[j][i];
                 }
- 
                 colMax = Math.max(colSum, colMax);
             }
  
-            // croSum 및 croMax 구하기
-            for (int i = 0; i < 100; i++) {
-                croSum1 += arr[i][i];
-                croSum2 += arr[i][99 - i];
+            for (int i = 0; i < map.length; i++) {
+                croSum1 += map[i][i];
+                croSum2 += map[i][99 - i];
             }
- 
             croMax = Math.max(croSum1, croSum2);
  
-            max = Math.max(rowMax, Math.max(colMax, croMax)); // rowMax, colMax, croMax 중 최댓값
+            int totalMax = Math.max(rowMax, Math.max(colMax, croMax));
  
-            bw.write("#" + tc + " " + max + "\n");
+            bw.write("#" + tc + " " + totalMax + "\n");
         }
  
         bw.flush();
