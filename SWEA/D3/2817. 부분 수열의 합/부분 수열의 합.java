d33d3394007534130d7dfda857f0fd7e0f391289
Yerim Lee <274205512+yerimlee-dev@users.noreply.github.com>
2026-04-15 01:42:30 +0900

    [D3] Title: 부분 수열의 합, Time: 157 ms, Memory: 25,856 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -3,46 +3,45 @@ import java.util.*;
  
 public class Solution {
     static int n;
-    static int k;
+    static int target;
     static int[] arr;
     static int count;
- 
+     
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
- 
+         
         int t = Integer.parseInt(br.readLine());
- 
+         
         for (int tc = 1; tc <= t; tc++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken()); // 원소 개수
-            k = Integer.parseInt(st.nextToken()); // 부분 수열의 합 기준
- 
+            n = Integer.parseInt(st.nextToken());
+            target = Integer.parseInt(st.nextToken());
             arr = new int[n];
-            count = 0; // 부분 수열의 합이 k인 개수
- 
+            count = 0;
+             
             st = new StringTokenizer(br.readLine());
             for (int i = 0; i < n; i++) {
                 arr[i] = Integer.parseInt(st.nextToken());
             }
- 
+             
             dfs(0, 0);
- 
+             
             bw.write("#" + tc + " " + count + "\n");
         }
- 
+         
         bw.flush();
     }
- 
+     
     public static void dfs(int idx, int sum) {
-        if (idx == n) { // idx가 n과 같을 때
-            if (sum == k) { // 부분 수열의 합이 k일 때
+        if (idx == n) {
+            if (sum == target) {
                 count++;
             }
- 
+             
             return;
         }
- 
+         
         dfs(idx + 1, sum + arr[idx]);
         dfs(idx + 1, sum);
     }
